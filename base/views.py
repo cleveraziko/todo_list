da3d0fb2fb5a1f82430ab04d41f2e5979f8d1425
cleveraziko <74561277+cleveraziko@users.noreply.github.com>
2021-10-03 16:44:38 +0500

    Class Based Views

---
+++ base/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from base.models import Task
+from django.urls import reverse_lazy
+
+# Create your views here.
+class TaskList(ListView):
+    model = Task
+
+class TaskDetail(DetailView):
+    model = Task
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields= '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields= '__all__'
+    success_url= reverse_lazy('tasks')
+
+class DeleteView (DeleteView):
+    model= Task
+    success_url = reverse_lazy('tasks')
