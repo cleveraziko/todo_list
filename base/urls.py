b78bbc8fea51aeeea26174d58ffe3837a871c046
cleveraziko <74561277+cleveraziko@users.noreply.github.com>
2021-10-05 11:21:05 +0500

    login

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/',views.CustomLoginView.as_view(), name='login' ),
     path('', views.TaskList.as_view() , name='tasks'),
     path('task/<int:pk>/', views.TaskDetail.as_view() , name='task'),
     path('task-create/', views.TaskCreate.as_view() , name='task-create'),
